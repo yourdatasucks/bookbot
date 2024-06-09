2024886df069699173e39f0ff6b36c8e27a4bd99
yourdatasucks <2204530+yourdatasucks@users.noreply.github.com>
2024-06-09 19:29:41 -0400

    created function to get text from file and created function for word count

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        first_line = file_contents.partition("\n")
-        print(f"The first line of the file is:\n{first_line[0]}")
-
-
+    book_location = "books/frankenstein.txt"
+    contents = open_book(book_location)
+    # first_line = contents.partition("\n")[0]
+    # print(f"First line is:\n{first_line}")
+    wc = word_count(contents)
+    print(f"This document contains {wc} words.")
+def open_book(path):
+    with open(path)as f:
+        return f.read()
+def word_count(text):
+    words = text.split()
+    return len(words)
 if __name__ == '__main__':
     main()
