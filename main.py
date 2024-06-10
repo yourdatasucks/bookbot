bc95812adeaec249d7987da62b9cd7e4e4643810
yourdatasucks <2204530+yourdatasucks@users.noreply.github.com>
2024-06-10 07:33:10 -0400

    created char_count function

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 def main():
     book_location = "books/frankenstein.txt"
     contents = open_book(book_location)
-    # first_line = contents.partition("\n")[0]
+    first_line = contents.partition("\n")[0]
     # print(f"First line is:\n{first_line}")
     wc = word_count(contents)
+    cc = char_count(contents)
     print(f"This document contains {wc} words.")
+    print(f"This document contains {cc}")
 def open_book(path):
     with open(path)as f:
         return f.read()
 def word_count(text):
     words = text.split()
     return len(words)
+def char_count(text):
+    chars_dict = {} # String(character) -> Integer(num_times_appeared)
+    words = text.split()
+    for word in words:
+        for char in word:
+            char = char.lower()
+            if char in chars_dict:
+                chars_dict[char] += 1
+            else:
+                chars_dict[char] = 1
+    return chars_dict
 if __name__ == '__main__':
     main()
